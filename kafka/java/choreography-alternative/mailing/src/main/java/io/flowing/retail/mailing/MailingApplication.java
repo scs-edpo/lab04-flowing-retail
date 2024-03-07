f1af85e428c133b2402d33d5eda32ac5b68618c0
Yasmin Lützelschwab <114688402+luetzyas@users.noreply.github.com>
2024-03-07 11:40:02 +0100

    add mailing microservice to choreography-alternative

---
+++ kafka/java/choreography-alternative/mailing/src/main/java/io/flowing/retail/mailing/MailingApplication.java
@@ -0,0 +1,13 @@
+package io.flowing.retail.mailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingApplication {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(MailingApplication.class, args);
+  }
+
+}
